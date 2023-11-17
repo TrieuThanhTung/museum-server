907be2eab85349a5c2b1e6e536f66c27c206dd67
TrieuThanhTung <tung.ttrieu@gmail.com>
2023-11-17 15:55:23 +0700

    add: update profile, change password

--- controlllers/SiteController.js
+++ controlllers/SiteController.js
@@ -35,7 +35,65 @@ class SiteController {
             console.log(error)
         }
     }
+
+    // user
+
+    async getUser(req, res) {
+        const id = req.params.id
+        try {
+            const response = await User.findById(id, 
+                {email: 1, name: 1, age: 1, gender: 1})
+            if(response) {
+                res.json(response)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async updateProfile(req, res) {
+        const id = req.params.id;
+        const data = req.body;
+
+        try {
+            if(data.password !== null) {
+                res.json({message: "Cannot change password by update profile"})
+                return;
+            }
+
+            const response = await User.findByIdAndUpdate(id, data, {
+                fields: {email: 1, name: 1, age: 1, gender: 1},
+                new: true,
+            })
+            if(response) {
+                res.json(response)
+                console.log("update profile succesfully")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async changePassword(req, res) {
+        const id = req.params.id;
+        const oldPassword = req.body.oldPassword
+        const password = req.body.password;
+        try {
+            const response = await User.findOneAndUpdate({_id: id, password: oldPassword}, {password: password}, {
+                new: true,
+            })
+            if(response) {
+                res.json({message: "change password successfully"})
+            } else {
+                res.json({message: "change password fail"})
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
+
+    // like
     async like(req, res) {
         const id = req.params.id
         const data = req.body;
